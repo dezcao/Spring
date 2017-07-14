412a7d5bf114162016ba16d1d28f38af425da47a
Win7 <Win7@PC036>
2017-07-14 21:46:53 +0900

    X-Frame-Options 해결 test필터 수정함.

--- sinzoro2/src/main/java/com/sinzoro/common/common/CorsFilter.java
+++ sinzoro2/src/main/java/com/sinzoro/common/common/CorsFilter.java
@@ -1,26 +1,25 @@
 package com.sinzoro.common.common;
 
 import java.io.IOException;
-
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TestFilter implements Filter {
+public class CorsFilter implements Filter {
     
-    private static final Logger logger = LoggerFactory.getLogger(TestFilter.class);
+    private static final Logger logger = LoggerFactory.getLogger(CorsFilter.class);
     
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-	    HttpServletRequest request = (HttpServletRequest)req;
-	    logger.debug("RequestURL : {} ", request.getRequestURL());
+	    HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("X-Frame-Options", "ALLOW-FROM http://58.151.24.166:8080");
 	    chain.doFilter(req, res);
 	}
 
