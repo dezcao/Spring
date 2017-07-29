51a784a15bbfa0c72147cc9ce1e9d21e74ae0aba
Hwang Jae Pil <dezcao@users.noreply.github.com>
2017-07-30 02:31:02 +0900

    Update Message.java

--- sinzoro2/src/main/java/com/sinzoro/common/common/Message.java
+++ sinzoro2/src/main/java/com/sinzoro/common/common/Message.java
@@ -3,9 +3,9 @@ package com.sinzoro.common.common;
 public enum Message {
     
     DATABASE_ERROR(0, "A database error has occured."),
-    WRONG_PASSWORD(0, "Incorrect Password."),
-    DUPICATE_USER(0, "This user already exists."),
-    LOGOUT_SUCCESS(0, "You've been logged out successfully.");
+    WRONG_PASSWORD(1, "Incorrect Password."),
+    DUPICATE_USER(2, "This user already exists."),
+    LOGOUT_SUCCESS(3, "You've been logged out successfully.");
     
     private int code;
     private String description;
